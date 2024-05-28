623737fae75919f5f4423dcf0b4d430f2541cfca
Jorge Morales <luifer991@gmail.com>
2024-05-28 14:40:32 -0500

    managing consulta by the controller

---
+++ src/main/java/med/voll/api/controller/ConsultaController.java
@@ -0,0 +1,28 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.consulta.AgendaDeConsultasService;
+import med.voll.api.domain.consulta.DatosAgemdarConsulta;
+import med.voll.api.domain.consulta.DatosDetalleConsulta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/consultas")
+public class ConsultaController {
+    
+    @Autowired
+    private AgendaDeConsultasService service;
+    
+    @PostMapping
+    @Transactional
+    public ResponseEntity agendar ( @RequestBody @Valid DatosAgemdarConsulta datos ) {
+        service.agendar(datos);
+        return ResponseEntity.ok(new DatosDetalleConsulta(null, null, null, null));
+    }
+}
